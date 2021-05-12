470cd8a2562ece57759fcfe7c2f2a2c686f23f38
wt <1546766776@qq.com>
2021-05-12 18:42:34 +0800

    提交更新记录

--- study_project/src/main/java/com/xu/pojo/User.java
+++ study_project/src/main/java/com/xu/pojo/User.java
@@ -1,16 +1,32 @@
 package com.xu.pojo;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    private Integer id;
+
+    // 对应数据库中的主键（uuid、自增id、雪花算法、redis、zookeeper!）
+    @TableId(type = IdType.AUTO) //input类型需要手动输入
+    private Long id;
     private String name;
     private Integer age;
     private String email;
+
+    // 字段添加填充内容
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
 
 }
--- study_project/src/test/java/com/xu/StudyMybatisPlusApplicationTests.java
+++ study_project/src/test/java/com/xu/StudyMybatisPlusApplicationTests.java
@@ -22,7 +22,30 @@ class StudyMybatisPlusApplicationTests {
         //查询全部用户
         List<User> users = userMapper.selectList(null);
         users.forEach(System.out::println);
-        System.out.println(users.toString());
+    }
+
+    //测试插入（insert）
+//    @Test
+//    public void testInsert(){
+//        User user = new User();
+//        user.setName("e3");
+//        user.setAge(13);
+//        user.setEmail("14s.com");
+//        int result = userMapper.insert(user);
+//        System.out.println(result);
+//    }
+
+    //测试更新（update）
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        // 通过条件自动拼接动态sql
+        user.setId((long) 45);
+        user.setName("e手动");
+        user.setAge(18);
+        // 注意：updateById 的参数是一个对象！
+        int result = userMapper.updateById(user);
+        System.out.println(result);
     }
 
 }
