262dd58d4b016deddf0a783d24dd845f70505b7d
wt <1546766776@qq.com>
2021-05-18 09:31:13 +0800

    提交更新记录

--- study_project/src/test/java/com/xu/WrapperTest.java
+++ study_project/src/test/java/com/xu/WrapperTest.java
@@ -31,7 +31,7 @@ public class WrapperTest {
         System.out.println(users);
     }
 
-    @Test
+//    @Test
     public void wrapperSelectByName(){
         // 根据name查询一条用户
         QueryWrapper<User> wrapper = new QueryWrapper<>();
@@ -39,4 +39,26 @@ public class WrapperTest {
         User user = userMapper.selectOne(wrapper); // 查询一条数据
         System.out.println(user);
     }
+
+//    @Test
+    public void wrapperSelectFilter(){
+        // 查询年龄在10到25之间的用户
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.between("age",10,25);
+        List<User> users = userMapper.selectList(wrapper);
+        System.out.println(users);
+
+    }
+
+    @Test
+    public void selectBySql(){
+        // 通过sql语句查询
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.inSql("age","select age from user where age < 30");
+//        List<Object> objects = userMapper.selectObjs(wrapper);
+        List<User> users = userMapper.selectList(wrapper);
+        System.out.println(users);
+
+    }
+
 }
